a4fb3438d2247006b4589658599948a9be1600a0
lucy <lucynottonson@icloud.com>
2025-03-16 20:41:16 -0700

    ugh

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 import { useState } from "react";
-import { supabase } from "@/lib/supabase";
-
 
 export default function RegisterPage() {
   const [formData, setFormData] = useState({ email: "", password: "" });
